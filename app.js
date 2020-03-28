31dd25444e3012655684f521cdb251a2f3c06b26
Josh Amore <joshamore31@gmail.com>
2020-03-28 14:13:15 +1100

    added SQLite3 DB for testing

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 const express = require("express");
+// TODO: Below is for dev only remove before deploying
+const sqlite3 = require("sqlite3").verbose();
 
 // Creates express server
 const app = express();
@@ -17,7 +19,34 @@ app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
 /*
 ----------- HELPER FUNCTIONS START -----------
 */
-const todo;
+let todo;
 /*
 ----------- HELPER FUNCTIONS End -----------
 */
+
+// DB testing
+let db = new sqlite3.Database("MyTinnies.db", sqlite3.OPEN_READWRIT, err => {
+	if (err) {
+		return console.error(err.message);
+	} else {
+		console.log("Connected to the in-memory SQlite database.");
+	}
+});
+
+// SQL query
+let sql = "SELECT * FROM tinnies WHERE user_ID=23";
+
+// Getting each row
+db.each(sql, (err, row) => {
+	if (err) {
+		console.error("REEE");
+	}
+	console.log(`User ${row.user_id} has ${row.tinnies} tinnies`);
+});
+
+db.close(err => {
+	if (err) {
+		console.error(err.message);
+	}
+	console.log("Close the database connection.");
+});
---
+++ dev/tinnesTableSQLite.sql
@@ -0,0 +1,7 @@
+/*
+    Statement to create the tinnies SQLite3 table (NOTE: this is a testing only table not designed for deployment).
+*/
+CREATE TABLE tinnies (
+    user_id INTEGER PRIMARY KEY,
+    tinnies INTEGER
+);

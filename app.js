723921ee84b8911075bfe56c1f4cd2c2149c73aa
Josh Amore <joshamore31@gmail.com>
2020-03-28 13:28:36 +1100

    added helper function section this will be moved to a dedicated file in the future

--- app.js
+++ app.js
@@ -13,3 +13,11 @@ const PORT = process.env.PORT || 5000;
 
 // Starting server
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+
+/*
+----------- HELPER FUNCTIONS START -----------
+*/
+const todo;
+/*
+----------- HELPER FUNCTIONS End -----------
+*/

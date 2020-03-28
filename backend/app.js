6a167793ae0a64f4654b88830e7fb8244a986eac
Josh Amore <joshamore31@gmail.com>
2020-03-28 12:58:02 +1100

    updated express initial layout

---
+++ backend/app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const path = require('path');
+
+require('dotenv').config()
+
+// Creates express app
+const app = express();
+
+// Pull in routes file.
+app.use('/api/', require('./routes/api/userCalls'));
+
+// Serves static files
+app.use(express.static('public'))
+
+// Uses port from environment or 500 if none.
+const PORT = process.env.PORT || 5000;
+
+// Starts server
+app.listen(PORT, () => console.log(`Server started on port: ${PORT}`));

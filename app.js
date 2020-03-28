f893b6d1efe48c6491555f2b9e9c625466819997
Josh Amore <joshamore31@gmail.com>
2020-03-28 14:39:56 +1100

    creating mockup

--- app.js
+++ app.js
@@ -2,6 +2,13 @@ const express = require("express");
 // TODO: Below is for dev only remove before deploying
 const sqlite3 = require("sqlite3").verbose();
 
+// TODO: for mock test -- delete
+const readline = require("readline");
+const rl = readline.createInterface({
+	input: process.stdin,
+	output: process.stdout
+});
+
 // Creates express server
 const app = express();
 
@@ -25,28 +32,58 @@ let todo;
 */
 
 // DB testing
-let db = new sqlite3.Database("MyTinnies.db", sqlite3.OPEN_READWRIT, err => {
-	if (err) {
-		return console.error(err.message);
+function dbStuff() {
+	let db = new sqlite3.Database("MyTinnies.db", sqlite3.OPEN_READWRIT, err => {
+		if (err) {
+			return console.error(err.message);
+		} else {
+			console.log("Connected to the SQlite database.");
+		}
+	});
+
+	// SQL query
+	let sql = "SELECT * FROM tinnies WHERE user_ID=23";
+
+	// Getting each row
+	db.each(sql, (err, row) => {
+		if (err) {
+			console.error("REEE");
+		}
+		console.log(`User ${row.user_id} has ${row.tinnies} tinnies`);
+	});
+
+	db.close(err => {
+		if (err) {
+			console.error(err.message);
+		}
+		console.log("Close the database connection.");
+	});
+}
+
+//Creating a CLI mock of app.
+
+// This global represents an auth item containing uid
+const USER = 26;
+
+console.log("Did you by a slab of tinnies? [Y/N]");
+
+rl.on("line", input => {
+	if (input === "y") {
+		mock.getTinnies();
 	} else {
-		console.log("Connected to the in-memory SQlite database.");
+		console.log("nerd");
 	}
 });
 
-// SQL query
-let sql = "SELECT * FROM tinnies WHERE user_ID=23";
+mock = {
+	getTinnies: () => {
+		console.log("How many tinnes bro?");
 
-// Getting each row
-db.each(sql, (err, row) => {
-	if (err) {
-		console.error("REEE");
+		rl.on("line", input => {
+			const inputInt = parseInt(input);
+		});
+	},
+	addTinnies: () => {
+		// TODO: query DB and check if the global user exists in DB. If so, update tinnies. If not, create record.
 	}
-	console.log(`User ${row.user_id} has ${row.tinnies} tinnies`);
-});
-
-db.close(err => {
-	if (err) {
-		console.error(err.message);
-	}
-	console.log("Close the database connection.");
-});
+};

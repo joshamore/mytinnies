c5596d395b7a06dc07d2414e53521c42a6ecf299
Josh Amore <joshamore31@gmail.com>
2020-04-08 12:20:05 +1000

    updated DB schema

--- dev/tinnesTableSQLite.sql
+++ dev/tinnesTableSQLite.sql
@@ -1,7 +1,9 @@
 /*
     Statement to create the tinnies SQLite3 table (NOTE: this is a testing only table not designed for deployment).
+    The user_id needs to come from the users table (i.e the user must be created first)
 */
-CREATE TABLE tinnies (
-    user_id INTEGER PRIMARY KEY,
-    tinnies INTEGER
+CREATE TABLE "tinnies" (
+	"record_id"	INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
+	"user_id"	INTEGER NOT NULL UNIQUE,
+	"tinnies"	INTEGER
 );
--- dev/usersTableSQLite.sql
+++ dev/usersTableSQLite.sql
@@ -2,8 +2,7 @@
     Statement to create the users SQLite3 table (NOTE: this is a testing only table not designed for deployment).
 */
 CREATE TABLE "users" (
-	"record_id"	INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
-	"user_id"	INTEGER UNIQUE,
+	"user_id"	INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
 	"first_name"	TEXT,
 	"last_name"	TEXT,
 	"email"	TEXT UNIQUE,

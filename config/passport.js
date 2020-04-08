eaa1d264d226be2a2ef7d904a25181e9a36491ff
Josh Amore <joshamore31@gmail.com>
2020-04-08 19:51:01 +1000

    finalised passport.js config file

--- config/passport.js
+++ config/passport.js
@@ -1,26 +1,70 @@
+const sqlite3 = require("sqlite3").verbose();
 const LocalStrategy = require("passport-local").Strategy;
 const userHelpers = require("./helpers/userHelpers");
 
-/*
-    Guide for SQLITE setup: https://stackoverflow.com/questions/23481817/node-js-passport-autentification-with-sqlite
-*/
-
-// Will need to
-USER = {
-	email: "bob@example.com",
-	password_plain:
-		"$2a$10$ob7sWNAfJPPq5SnBJazcyuUJ0BAXFSMeagqwxwuuhcKOZ7LD02OHa",
-	password_hash: "reeeeeeeeeee",
-};
+// Opening DB connection
+let db = new sqlite3.Database(
+	"../MyTinnies.db",
+	sqlite3.OPEN_READONLY,
+	(err) => {
+		if (err) {
+			throw err;
+		} else {
+			console.log("Connected to the SQlite database.");
+		}
+	}
+);
 
 module.exports = function (passport) {
 	passport.use(
-		new LocalStrategy({ usernameField: "email" }, (email, password, done) => {
-			// Matching user - Note: will need to call db, but dor now it's just
-			// checking the array USER
-			if (email !== USER.email) {
-				return done(null, false, { message: "User email does not exist" });
-			}
+		new LocalStrategy((email, password, done) => {
+			// Checking if user's email exists in DB.
+			db.get("SELECT * FROM users WHERE email = ?", email, (err, row) => {
+				if (!row) {
+					return done(null, false, { message: "User email not found" });
+				} else {
+					// Checking if password hash matches
+					userHelpers
+						.passwordHash(password)
+						.then((res, rej) => {
+							db.get(
+								"SELECT email, user_id FROM users WHERE email = ? AND password_hash = ?",
+								email,
+								res,
+								(err, row) => {
+									if (err) throw err;
+
+									if (!row) {
+										return done(null, false, { message: "Password incorrect" });
+									} else {
+										return done(null, row);
+									}
+								}
+							);
+						})
+						.catch((err) => {
+							throw err;
+						});
+				}
+			});
 		})
 	);
+
+	passport.serializeUser(function (user, done) {
+		return done(null, user.user_id);
+	});
+
+	passport.deserializeUser(function (id, done) {
+		db.get(
+			"SELECT user_id, email FROM users WHERE user_id = ?",
+			id,
+			(err, row) => {
+				if (!row) {
+					return done(null, false);
+				} else {
+					return done(null, row);
+				}
+			}
+		);
+	});
 };

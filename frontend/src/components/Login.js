e2ba6a6b79537b50602e6db2a5008f4c62b4f1ed
Josh Amore <joshamore31@gmail.com>
2020-04-13 09:43:07 +1000

    login form sending data to server

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -15,33 +15,28 @@ export class Login extends Component {
 		this.setState({ [input]: e.target.value });
 	};
 
-	// Attempts to log in
+	// Attempting login
 	login = (e) => {
 		e.preventDefault();
 
-		// TODO: fix the below -- this is not sending the data to the server (seems to be an issue with fetch POST method)
-		// Creating a form object
-		let formData = new FormData();
-		formData.append("email", this.state.email);
-		formData.append("password", this.state.password);
+		// Setting form data to a URLSearchParams object
+		const searchParamsForm = new URLSearchParams();
+		searchParamsForm.set("email", this.state.email);
+		searchParamsForm.set("password", this.state.password);
 
-		console.log(formData);
+		// Submitting form
 		fetch("http://localhost:5000/users/login/", {
 			method: "POST",
-			body: formData,
+			body: searchParamsForm,
 		})
-			.then((dataProm) => {
-				dataProm
-					.json()
-					.then((data) => {
-						// Printing success message to console.
-						console.log(data);
-					})
-					.catch((e) => {
-						throw e;
-					});
+			.then((data) => {
+				// TODO: this should redirect
+				console.log(data);
 			})
-			.catch((e) => console.log(e.message + " reeeee!"));
+			.catch((e) => {
+				// TODO: this should reroute to failed page or throw an alert
+				console.log(e.message + " Failed!");
+			});
 	};
 
 	render() {

5c69093467ae41d41f87da293e9b459b4dfcd85d
Josh Amore <joshamore31@gmail.com>
2020-04-13 17:47:32 +1000

    setup register and login views

--- README.md
+++ README.md
@@ -11,3 +11,9 @@ Drive past and risk running out? Yeah, nah.
 **You check Tinnies.**
 
 Tinnies quickly tells you how many cans are in the fridge. Six left!? Yeah mate, you'll need to grab a slab (or two -- aren't the boys coming around tonight after Wayne finshes building his deck?).
+
+## Tools ref
+
+- Bootstrap 4: https://getbootstrap.com/docs/4.4/getting-started/introduction/
+- Bootswatch theme: https://bootswatch.com/sketchy/
+- Fontawesome: https://fontawesome.com/v4.7.0/get-started/
--- routes/users.js
+++ routes/users.js
@@ -7,32 +7,67 @@ const userHelpers = require("../helpers/userHelpers");
 // Register user handle
 router.post("/register", (req, res) => {
 	// Storing email and password from request body
-	const { firstName, lastName, email, password } = req.body;
+	const { firstName, lastName, email, password, password2 } = req.body;
 
-	// Confirming if user already exists
-	userHelpers
-		.checkUserExistsByEmail(email)
-		.then((confirm) => {
-			// If user already exists, respond with error
-			if (confirm) {
-				throw Error("Email address already registered");
-			} else {
-				// Creating new user
-				userHelpers
-					.createNewUser(firstName, lastName, email, password)
-					.then((newUser) =>
-						// TODO: This is a placeholder -- will need a different success response/process when
-						// frontend exists.
-						res.send({
-							success: `New user created with ID ${newUser}`,
-						})
-					)
-					.catch((err) => {
-						res.send({ error: err.message });
-					});
-			}
-		})
-		.catch((err) => res.send({ error: err.message }));
+	// Storing any errors
+	let errors = [];
+
+	// Checking required fields aren't blank
+	if (!firstName || !lastName || !email || !password || !password2) {
+		errors.push({ msg: "Please fill in all fields" });
+	}
+
+	// Validates that both passwords match
+	if (password !== password2) {
+		errors.push({ msg: "Passwords must match" });
+	}
+
+	// Validate password length
+	if (password.length < 6) {
+		errors.push({ msg: "Password must be at least 6 characters" });
+	}
+
+	// Rerendering with errors if any present or continuing to DB calls
+	if (errors.length > 0) {
+		res.render("register", {
+			errors,
+			firstName,
+			lastName,
+			email,
+			password,
+			password2,
+		});
+	} else {
+		// Confirming if user already exists
+		userHelpers
+			.checkUserExistsByEmail(email)
+			.then((confirm) => {
+				// If user already exists, respond with error
+				if (confirm) {
+					throw Error("Email address already registered");
+				} else {
+					// Creating new user
+					userHelpers
+						.createNewUser(firstName, lastName, email, password)
+						.then((newUser) =>
+							// TODO: This is a placeholder -- will need a different success response/process when
+							// frontend exists.
+							res.send({
+								success: `New user created with ID ${newUser}`,
+							})
+						)
+						.catch((err) => {
+							res.send({ error: err.message });
+						});
+				}
+			})
+			.catch((err) => res.send({ error: err.message }));
+	}
+});
+
+// Register route
+router.get("/register", (req, res) => {
+	res.render("register");
 });
 
 // Login handle
@@ -41,6 +76,11 @@ router.post("/login", (req, res, next) => {
 		successRedirect: "/users/login",
 		failureRedirect: "/users/fail",
 	})(req, res, next);
+});
+
+// Login route
+router.get("/login", (req, res) => {
+	res.render("login");
 });
 
 // Logout handle
@@ -53,6 +93,11 @@ router.post("/logout", (req, res) => {
 		// Redirects user to homepage if they're not logged in
 		res.redirect("/");
 	}
+});
+
+// Logout route
+router.get("/logout", (req, res) => {
+	res.send("yeah mate, just left the pub");
 });
 
 // Auth test route

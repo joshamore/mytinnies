5d64bbc41dacf227c9136620f120e07e8ca80b10
Josh Amore <joshamore31@gmail.com>
2020-04-15 12:53:45 +1000

    adding tinnies to dashboard

--- public/scripts/scripts.js
+++ public/scripts/scripts.js
@@ -1,9 +1,31 @@
 const getters = {
-    getTinnies = function() {
-        const url = "localhost:5000/api/getTinnies";
+	getTinnies: async function () {
+		const url = "http://localhost:5000/api/getTinnies";
 
-        fetch(url).then(res => {
-            console.log(res);
-        }).catch(e => console.log(e.message));
-    }
-}   
+		try {
+			let response = await fetch(url);
+			if (response.redirected === true) {
+				return Error("NO Auth");
+			} else {
+				return await response.json();
+			}
+		} catch (err) {
+			console.error(err);
+			return Error("Failed to get tinnies");
+		}
+	},
+};
+
+function authTest() {
+	const url = "http://localhost:5000/users/auth";
+	fetch(url);
+}
+
+document.onload = getters
+	.getTinnies()
+	.then((tinnies) => {
+		document.getElementById("tinnies").innerHTML = tinnies.tinnies;
+	})
+	.catch((err) => {
+		console.error(err.message);
+	});

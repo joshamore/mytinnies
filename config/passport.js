07dc207482eaeb248d3db2200022aefc6a90db06
Josh Amore <joshamore31@gmail.com>
2020-04-23 14:55:28 +1000

    removed SQLite code

--- config/passport.js
+++ config/passport.js
@@ -2,28 +2,27 @@ const { Pool, Client } = require("pg");
 const LocalStrategy = require("passport-local").Strategy;
 const userHelpers = require("../helpers/userHelpers");
 
+// Connecting to PG database
+const pool = new Pool({
+	user: process.env.PG_USER,
+	host: process.env.PG_URL,
+	database: process.env.DB_NAME,
+	password: process.env.PG_PASSWORD,
+	port: process.env.PG_PORT,
+});
+
 module.exports = function (passport) {
-	// Connecting to PG database
-	const pool = new Pool({
-		user: process.env.PG_USER,
-		host: process.env.PG_URL,
-		database: process.env.DB_NAME,
-		password: process.env.PG_PASSWORD,
-		port: process.env.PG_PORT,
-	});
 	passport.use(
 		new LocalStrategy({ usernameField: "email" }, (email, password, done) => {
-			// Creating record in users table
+			// Getting record from users table
 			pool.query(
 				"SELECT * FROM users WHERE email=$1",
 				[email],
 				(err, userData) => {
 					if (err) {
-						pool.end();
 						console.log(err);
 						return done(null, false, { message: "Issue with user. Failed." });
 					} else if (userData.rowCount === 0) {
-						pool.end();
 						return done(null, false, { message: "User email not found" });
 					} else {
 						// Validating password
@@ -42,26 +41,6 @@ module.exports = function (passport) {
 					}
 				}
 			);
-			// // Checking if user's email exists in DB.
-			// db.get("SELECT * FROM users WHERE email = ?", email, (err, row) => {
-			// 	if (!row) {
-			// 		return done(null, false, { message: "User email not found" });
-			// 	} else {
-			// 		// Checking if password matches
-			// 		userHelpers
-			// 			.passwordHashCompare(password, row.password_hash)
-			// 			.then((isMatch) => {
-			// 				if (!isMatch) {
-			// 					return done(null, false, { message: "Password incorrect" });
-			// 				} else {
-			// 					return done(null, { email: row.email, user_id: row.user_id });
-			// 				}
-			// 			})
-			// 			.catch((err) => {
-			// 				throw err;
-			// 			});
-			// 	}
-			// });
 		})
 	);
 
@@ -70,41 +49,18 @@ module.exports = function (passport) {
 	});
 
 	passport.deserializeUser((id, done) => {
-		// Connecting to PG database
-		const pool = new Pool({
-			user: process.env.PG_USER,
-			host: process.env.PG_URL,
-			database: process.env.DB_NAME,
-			password: process.env.PG_PASSWORD,
-			port: process.env.PG_PORT,
-		});
-
 		pool.query(
 			"SELECT user_id, email FROM users WHERE user_id=$1",
 			[id],
 			(err, userData) => {
 				if (err) {
-					pool.end();
 					return done(null, false);
 				} else if (userData.rowCount === 0) {
-					pool.end();
 					return done(null, false);
 				} else {
-					pool.end();
 					return done(null, userData.rows[0]);
 				}
 			}
 		);
-		// db.get(
-		// 	"SELECT user_id, email FROM users WHERE user_id = ?",
-		// 	id,
-		// 	(err, row) => {
-		// 		if (!row) {
-		// 			return done(null, false);
-		// 		} else {
-		// 			return done(null, row);
-		// 		}
-		// 	}
-		// );
 	});
 };

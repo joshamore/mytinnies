e114204e7b9f070476a30d477da564ab7a6f4b23
Josh Amore <joshamore31@gmail.com>
2020-04-23 14:47:07 +1000

    removed SQLite code.

--- app.js
+++ app.js
@@ -14,16 +14,12 @@ require("dotenv").config();
 require("./config/passport")(passport);
 
 /*
-	NOTE RE testing DB: 
-		- User ID 1 has a plaintext password of: "reeeeeeeeeee"
-		- User ID 2 has a plaintext password of: "cool stuff"
 
+TODO: delete in prod
 	in PG:
 	user 1 password = testybruhyas
 */
 
-// TODO: Below is for dev only remove before deploying
-const sqlite3 = require("sqlite3").verbose();
 // Create express server
 const app = express();
 
@@ -75,13 +71,3 @@ const PORT = process.env.PORT || 5000;
 
 // Starting server
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
-
-/////////////// Testing for pg
-const dbHelpers = require("./helpers/dbHelpers");
-
-dbHelpers
-	.getUserHistory(1)
-	.then((data) => {
-		console.log(data);
-	})
-	.catch((err) => console.log(err));
--- helpers/dbHelpers.js
+++ helpers/dbHelpers.js
@@ -1,4 +1,3 @@
-const sqlite3 = require("sqlite3").verbose();
 const { Pool, Client } = require("pg");
 
 module.exports = {
@@ -42,34 +41,6 @@ module.exports = {
 					}
 				}
 			);
-
-			// // Opening DB connection
-			// let db = new sqlite3.Database(
-			// 	"./MyTinnies.db",
-			// 	sqlite3.OPEN_READWRITE,
-			// 	(err) => {
-			// 		if (err) {
-			// 			rej(Error("Unable to open DB"));
-			// 		} else {
-			// 			console.log("Connected to the SQlite database.");
-			// 		}
-			// 	}
-			// );
-
-			// // Query to create user record
-			// let sql =
-			// 	'INSERT INTO users ("first_name", "last_name", "email", "password_hash") VALUES (?, ?, ?, ?)';
-
-			// // Updating DB data and closing
-			// db.run(sql, [firstName, lastName, email, passwordHash], function (err) {
-			// 	if (err) {
-			// 		db.close();
-			// 		rej(Error("Unable to create user"));
-			// 	} else {
-			// 		db.close();
-			// 		res(this.lastID);
-			// 	}
-			// });
 		});
 	},
 	createUserTinniesRecord: (userID, tinnies) => {
@@ -77,7 +48,7 @@ module.exports = {
 			@args userID = the new user's ID
 			@args tinnies = the number of tinnies for new user
 
-			@returns a promise which will resolve with the tinnies record ID
+			@returns a promise which will resolve with the tinnies record ID. Otherwise, throws error.
 		*/
 		return new Promise((res, rej) => {
 			// Connecting to PG database
@@ -105,32 +76,6 @@ module.exports = {
 					res(userData.rows[0].record_id);
 				}
 			});
-			// // Opening DB connection
-			// let db = new sqlite3.Database(
-			// 	"./MyTinnies.db",
-			// 	sqlite3.OPEN_READWRITE,
-			// 	(err) => {
-			// 		if (err) {
-			// 			rej(Error("Unable to open DB"));
-			// 		} else {
-			// 			console.log("Connected to the SQlite database.");
-			// 		}
-			// 	}
-			// );
-			// // Query to create tinnies record
-			// let sql = 'INSERT INTO tinnies ("user_id", "tinnies") VALUES (?, ?)';
-			// // Updating DB data and closing
-			// db.run(sql, [userID, tinnies], function (err) {
-			// 	if (err) {
-			// 		db.close();
-			// 		rej(
-			// 			Error("Unable to create tinnies record (probably exists already)")
-			// 		);
-			// 	} else {
-			// 		db.close();
-			// 		res(this.lastID);
-			// 	}
-			// });
 		});
 	},
 	createUserHistoryRecord: function (userID, tinniesCount, posOrNeg) {
@@ -179,46 +124,6 @@ module.exports = {
 					}
 				);
 			}
-
-			// // Rejecting with error if invalid argument passed
-			// if (posOrNeg > 1 || posOrNeg < -2 || posOrNeg === 0) {
-			// 	rej(Error("posOrNeg value (3rd arg) must be -1 or 1"));
-			// } else if (tinniesCount <= 0) {
-			// 	rej(Error("tinnies count must be a positive number" + tinniesCount));
-			// } else {
-			// 	// Opening DB connection
-			// 	let db = new sqlite3.Database(
-			// 		"./MyTinnies.db",
-			// 		sqlite3.OPEN_READWRITE,
-			// 		(err) => {
-			// 			if (err) {
-			// 				rej(Error("Unable to open DB"));
-			// 			} else {
-			// 				console.log("Connected to the SQlite database.");
-			// 			}
-			// 		}
-			// 	);
-
-			// 	// Creating unix timestamp as string
-			// 	const timestamp = Math.floor(Date.now() / 1000).toString();
-
-			// 	// Query to create history record
-			// 	let sql =
-			// 		'INSERT INTO history ("user_id", "datetime", "pos_or_neg", "history_tinnies_count") VALUES (?, ?, ?, ?)';
-
-			// 	// Updating DB data and closing
-			// 	db.run(sql, [userID, timestamp, posOrNeg, tinniesCount], function (
-			// 		err
-			// 	) {
-			// 		if (err) {
-			// 			db.close();
-			// 			rej(Error("Unable to create history record"));
-			// 		} else {
-			// 			db.close();
-			// 			res(true);
-			// 		}
-			// 	});
-			// }
 		});
 	},
 	getUserTinniesData: (userID) => {
@@ -253,36 +158,6 @@ module.exports = {
 					res(userData.rows[0]);
 				}
 			});
-
-			// // Opening DB connection
-			// let db = new sqlite3.Database(
-			// 	"./MyTinnies.db",
-			// 	sqlite3.OPEN_READWRITE,
-			// 	(err) => {
-			// 		if (err) {
-			// 			rej(Error("Unable to open DB"));
-			// 		} else {
-			// 			console.log("Connected to the SQlite database.");
-			// 		}
-			// 	}
-			// );
-
-			// // Query to get user data from DB
-			// let sql = `SELECT * FROM tinnies WHERE user_ID=?`;
-
-			// // Getting DB data and closing
-			// db.get(sql, [userID], (err, row) => {
-			// 	if (err) {
-			// 		db.close();
-			// 		rej(Error("Unable to access user"));
-			// 	} else if (row === undefined) {
-			// 		db.close();
-			// 		rej(Error("User does not exist"));
-			// 	} else {
-			// 		db.close();
-			// 		res(row);
-			// 	}
-			// });
 		});
 	},
 	updateTinnies: (newTinnies, userID) => {
@@ -309,8 +184,6 @@ module.exports = {
 			pool.query(sql, [newTinnies, userID], (err, userData) => {
 				if (err) {
 					pool.end();
-					console.log(err);
-					console.log(`Got this ID: ${userID} and this tinnies: ${newTinnies}`);
 					rej(Error("Unable to access user tinnies record"));
 				} else if (userData.rowCount === 0) {
 					pool.end();
@@ -325,40 +198,13 @@ module.exports = {
 					}
 				}
 			});
-			// // Opening DB connection
-			// let db = new sqlite3.Database(
-			// 	"./MyTinnies.db",
-			// 	sqlite3.OPEN_READWRITE,
-			// 	(err) => {
-			// 		if (err) {
-			// 			rej(Error("Unable to open DB"));
-			// 		} else {
-			// 			console.log("Connected to the SQlite database.");
-			// 		}
-			// 	}
-			// );
-
-			// // Query to update user's tinnnies
-			// let sql = `UPDATE tinnies SET tinnies = ${newTinnies} WHERE user_id = ?`;
-
-			// // Updating DB data and closing
-			// db.run(sql, [user], (err) => {
-			// 	if (err) {
-			// 		db.close();
-			// 		rej(Error("Unable to access user"));
-			// 	} else {
-			// 		db.close();
-			// 		console.log(`New tinnies: ${newTinnies}`);
-			// 		res(true);
-			// 	}
-			// });
 		});
 	},
 	getUserFromEmail: (email) => {
 		/*
 			@args email = the user email address to retrieve from DB
 
-			@returns an object promise containing the user's data or an empty array if no user exists
+			@returns an object promise containing the user's data or an object with userNotFound === true
 		*/
 		return new Promise((res, rej) => {
 			// Connecting to PG database
@@ -388,43 +234,13 @@ module.exports = {
 					}
 				}
 			});
-
-			// // Opening DB connection
-			// let db = new sqlite3.Database(
-			// 	"./MyTinnies.db",
-			// 	sqlite3.OPEN_READWRITE,
-			// 	(err) => {
-			// 		if (err) {
-			// 			rej(Error("Unable to open DB"));
-			// 		} else {
-			// 			console.log("Connected to the SQlite database.");
-			// 		}
-			// 	}
-			// );
-
-			// // Query to get user data from DB from email address
-			// let sql = `SELECT * FROM users WHERE email=?`;
-
-			// // Getting DB data and closing
-			// db.get(sql, [email], (err, row) => {
-			// 	if (err) {
-			// 		db.close();
-			// 		rej(Error("Unable to access user record"));
-			// 	} else if (row === undefined) {
-			// 		db.close();
-			// 		res({ userNotFound: true });
-			// 	} else {
-			// 		db.close();
-			// 		res(row);
-			// 	}
-			// });
 		});
 	},
 	getUserHistory: function (user_ID) {
 		/*
 			@args user_ID = the user's ID.
 
-			@returns a promise that will resolve to an object containing the user's history
+			@returns a promise that will resolve to an object containing the user's history rows
 		*/
 		return new Promise((res, rej) => {
 			// Connecting to PG database
@@ -449,35 +265,6 @@ module.exports = {
 					res(userData.rows);
 				}
 			});
-			// // Opening DB connection
-			// let db = new sqlite3.Database(
-			// 	"./MyTinnies.db",
-			// 	sqlite3.OPEN_READWRITE,
-			// 	(err) => {
-			// 		if (err) {
-			// 			rej(Error("Unable to open DB"));
-			// 		} else {
-			// 			console.log("Connected to the SQlite database.");
-			// 		}
-			// 	}
-			// );
-
-			// // Query to get user history data from DB
-			// let sql = `SELECT * FROM history WHERE user_id=?`;
-
-			// // Getting DB data and closing
-			// db.all(sql, [user_ID], (err, rows) => {
-			// 	if (err) {
-			// 		db.close();
-			// 		rej(Error("Unable to access user record"));
-			// 	} else if (rows === undefined) {
-			// 		db.close();
-			// 		res({ userNotFound: true });
-			// 	} else {
-			// 		db.close();
-			// 		res(rows);
-			// 	}
-			// });
 		});
 	},
 };
